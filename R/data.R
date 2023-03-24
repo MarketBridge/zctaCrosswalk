c258facf0f26d446317f7ba79d26fb22f1c71836
Ari Lamstein <alamstein@market-bridge.com>
2023-03-24 10:52:39 -0700

    Add zcta_crosswalk data object to package.

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' 2020 Crosswalk of ZIP Code Tabulation Areas (ZCTAs)
+#'
+#' Retrieved via the function get_zcta_crosswalk. Columns are ZCTA, county_fips,
+#' county_name and state_fips.
+#'
+#' @docType data
+#' @name zcta_crosswalk
+#' @usage data(zcta_crosswalk)
+NULL
