60a23ef849a6452a4609435226b86b3d98bbec15
Ari Lamstein <arilamstein@gmail.com>
2023-04-07 21:02:55 -0400

    Add more columns to df and save with --resave-data because CRAN complains otherwise.

--- R/data.R
+++ R/data.R
@@ -1,7 +1,8 @@
 #' 2020 Crosswalk of ZIP Code Tabulation Areas (ZCTAs)
 #'
-#' Retrieved via the function get_zcta_crosswalk. Columns are ZCTA, county_fips,
-#' county_name and state_fips.
+#' The primary data was obtained via the function get_zcta_crosswalk. There are 3 types of columns: ZCTA,
+#' state and county. Where data in practice sometimes appears as both character and numeric, columns for both are 
+#' provided. 
 #'
 #' @docType data
 #' @name zcta_crosswalk
